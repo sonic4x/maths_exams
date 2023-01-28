46a1f1e5446db3a8f8260c8b53cf595596cde695
Liang Xue <liang.xue@bmw.com>
2023-01-28 13:51:29 +0800

    Fix eslint import/no-anonymous-default-export

--- frontend_react/src/global_v.js
+++ frontend_react/src/global_v.js
@@ -1,10 +1,8 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default
 {
     // if backend and frontend are bind together, you don't need to specify address here.
     // Otherwise, specify address.
     // api_server:"10.0.0.26:5000"
-    api_server:"maths.deta.dev",
-    dummy:"dummy"
-
-
+    api_server:"maths.deta.dev"
 }
