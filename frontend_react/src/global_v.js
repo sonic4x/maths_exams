cd2ceb8fa90d753c39425ca91c484e86b8878adc
Liang Xue <liang.xue@bmw.com>
2023-01-28 13:40:13 +0800

    test actions

--- frontend_react/src/global_v.js
+++ frontend_react/src/global_v.js
@@ -3,6 +3,8 @@ export default
     // if backend and frontend are bind together, you don't need to specify address here.
     // Otherwise, specify address.
     // api_server:"10.0.0.26:5000"
-    api_server:"maths.deta.dev"
+    api_server:"maths.deta.dev",
+    dummy:"dummy"
+
 
 }
