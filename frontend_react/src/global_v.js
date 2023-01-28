2dd818de6bf287a5f16d90464d9b4480c3266111
Liang Xue <liang.xue@bmw.com>
2023-01-28 15:02:11 +0800

    Modify request URL

--- frontend_react/src/global_v.js
+++ frontend_react/src/global_v.js
@@ -4,5 +4,5 @@ export default
     // if backend and frontend are bind together, you don't need to specify address here.
     // Otherwise, specify address.
     // api_server:"10.0.0.26:5000"
-    api_server:"maths.deta.dev"
+    api_server:"https://maths.deta.dev"
 }
