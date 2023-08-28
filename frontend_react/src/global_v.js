7451cdf1b4981b72b7a73d606e1532fb25e21a1a
sonic4x <leonxue.84@gmail.com>
2023-08-28 09:15:34 +0800

    Comment

--- frontend_react/src/global_v.js
+++ frontend_react/src/global_v.js
@@ -1,8 +1,9 @@
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default
 {
-    // if backend and frontend are bind together, you don't need to specify address here.
+    // if backend and frontend are bind together, you don't need to specify address here. like this:
+    // api_server:""
     // Otherwise, specify address.
-    // api_server:"10.0.0.26:5000"
-    api_server:"https://maths.deta.dev"
+    api_server:"10.0.0.26:5000"
+    // api_server:"https://maths.deta.dev"
 }
