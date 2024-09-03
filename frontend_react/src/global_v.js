4cab12d5cdfe2e51952e5b65216a0394fd5bbc65
sonic4x <sonic4x@163.com>
2024-09-03 22:56:25 +0800

    Modify url

--- frontend_react/src/global_v.js
+++ frontend_react/src/global_v.js
@@ -3,8 +3,10 @@ export default
 {
     // if backend and frontend are bind together, you don't need to specify address here. like this:
     // api_server:""
+    
     // Otherwise, specify address.
-    // api_server:"http://127.0.0.1:5000"
-    api_server:"http://10.0.0.18:5555"
+    api_server:"//" +window.location.hostname + ":5555"
+    // api_server:"http://127.0.0.1:5555"
+    // api_server:"http://10.0.0.18:5555"
     // api_server:"https://maths.deta.dev"
 }
